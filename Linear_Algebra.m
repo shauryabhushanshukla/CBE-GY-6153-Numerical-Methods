d2cd6b4ed1d6fd5b2aa59b7eaac6bf386d40760c
Shaurya Shukla <Shauryabhushanshukla@gmail.com>
2026-01-30 13:43:20 -0500

    Update Linear_Algebra.m

--- Linear_Algebra.m
+++ Linear_Algebra.m
@@ -1,19 +1,50 @@
-clc, clearvars, clear all
+function [] = laminar_flow()
 
-A = [1 1 1; 2 1 3; 3 1 6]
-b = [4; 7; 2]
-X = A\b   % A * X  = b
+% Constants:
+visc = 1e-3; %viscosity, Pa*s
+rho = 1e3; %density, kg/m3
+V_up = 5e-5; %velocity, m/s
+B = 1e-3; %distance, m
+dp_dx = -1; %Pa/m
 
 
-% % Dot product of complex vector
-% v1 = [1 + 2i; 2 - 1i; 3 + 0i]; % Define a complex vector
-% v2 = [4 - 1i; 5 + 2i; 6 + 3i]; % Define another complex vector
-% 
-% result  = v1.^v2
-% % dotProduct = dot(v1, v2); % Compute the dot product
-% % disp(dotProduct)
 
-v1 = [2, 3, 4];
-v2 = [2, 3, 2];
-result = v1.^v2 % Result: [4, 27, 16]
+%Grid Parameters:
+N = 1000; %Number of grid points
+dy = B/(N+1); %grid spacing, m
+G = -dy^2/visc*dp_dx;
 
+
+%Define matrix
+
+A = spalloc(N,N,3*N-2);
+b = zeros(N,1);
+
+j = 1;
+A(1,1) = 2;
+A(1,2) = -1;
+b(1) = G;
+
+for j=2:N-1
+    A(j,j-1)=-1;
+    A(j,j)=2;
+    A(j,j+1)=-1;
+    b(j)=G;
+end
+
+j=N;
+A(N,N-1)=-1;
+A(N,N)=2;
+b(N)=G+V_up;
+
+v=A\b;
+
+y = linspace(dy,B-dy,N)
+
+v_anal = V_up*y/B + 1/2/visc*dp_dx*(y.^2-B*y);
+
+plot(y,v_anal,y,v,'.')
+xlabel('y [m]')
+ylabel('v [m/s]')
+legend('v_a_n_a_l','v_f_d')
+end
